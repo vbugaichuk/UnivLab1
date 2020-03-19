2a93cb282104f7ca60d4b6263976e7f96e965f20
vbugaichuk <valikbug@gmail.com>
2020-03-19 21:08:01 +0200

    7 етап

--- Models/Country.cs
+++ Models/Country.cs
@@ -13,6 +13,7 @@ namespace DBIT
 
         public int Id { get; set; }
         [Required(ErrorMessage ="Поле не повинно бути порожнім")]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="Довжина рядка повина бути від 3 до 50 символів")]
         [Display(Name = "Країна")]
         public string Name { get; set; }
         [Display(Name = "Інформація про країну")]
--- Models/Employer.cs
+++ Models/Employer.cs
@@ -13,10 +13,21 @@ namespace DBIT
         [Required(ErrorMessage = "Поле не повинно бути порожнім")]
         [Display(Name = "Працівник")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
         public string Passport { get; set; }
+
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Range(1900, 2020, ErrorMessage = "Недопустимий рік")]
         public int Year { get; set; }
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
         public string Education { get; set; }
+
 
         public virtual ICollection<EmpSub> EmpSubs { get; set; }
     }
